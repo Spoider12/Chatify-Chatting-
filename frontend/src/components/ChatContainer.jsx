737a4677517bd2073fcc6a157725203f2acc79f4
Spoider12 <153114166+Spoider12@users.noreply.github.com>
2025-12-20 17:21:40 +0530

    chat container setup implemented

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -3,9 +3,12 @@ import {useChatStore} from '../store/useChatStore'
 import {useAuthStore} from '../store/useAuthStore'
 import ChatHeader from './ChatHeader';
 import NoChatHistoryPlaceholder from './NoChatHistoryPlaceholder';
+import MessageInput from './MessageInput';
+import MessageLoadingSkeleton from './MessageLoadingSkeleton';
+
 
 function ChatContainer() {
-  const {selectedUser,getMessagesByUserId,messages} = useChatStore();
+  const {selectedUser,getMessagesByUserId,messages,isMessagesLoading} = useChatStore();
   const {authUser} = useAuthStore();
   useEffect(()=>{
     getMessagesByUserId(selectedUser._id);
@@ -14,12 +17,32 @@ function ChatContainer() {
     <>
     <ChatHeader/>
     <div className='flex-1 px-6 overflow-y-auto py-8'>
-    {messages.length > 0 ? (
-      <p>Some Messages</p>
-    ) : (
+    {messages.length > 0 && !isMessagesLoading ? (
+      <div className='max-w-3xl mx-auto space-y-6'>
+        {messages.map(msg =>(
+          <div key={msg._id}>
+            className={`chat ${msg.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+            <div className={`chat-bubble relative ${
+              msg.senderId === authUser._id ? "bg-cyan-500 text-white" : "bg-slate-700 text-slate-200"
+            }`}>
+              {msg.image && (
+                <img src={msg.image} alt="Sent Image" className="rounded-lg h-48 object-cover" />
+              )}
+              {msg.text && <p className='mt-2'>{msg.text}</p>}
+              <p className='text-xs mt-1 opacity-75 flex items-center gap-1'>
+                {new Date(msg.createdAt).toISOString().slice(11,16)}
+              </p>
+            </div>
+
+          </div>
+        ))}
+
+      </div>
+    ) : isMessagesLoading ? <MessageLoadingSkeleton />  : (
       <NoChatHistoryPlaceholder name={selectedUser.fullName}/>
     )}
     </div>
+    <MessageInput />
     </>
   )
 }
