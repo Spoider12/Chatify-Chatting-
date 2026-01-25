e92a090a93b02717a446d7e2d089384cc71b3e66
Spoider12 <153114166+Spoider12@users.noreply.github.com>
2026-01-25 15:41:52 +0530

    Update API URLs and CORS configuration with correct Render and Vercel domains

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json({ limit: "5mb" })); // req.body
-app.use(cors({ origin: "https://chatify-chatting-dyrh.vercel.app", credentials: true }));
+app.use(cors({ origin: "https://chatify-chatting.vercel.app", credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
