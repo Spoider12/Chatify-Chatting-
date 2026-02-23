90b94a3af722edf086205bbfb5656c69bc47460b
Spoider12 <153114166+Spoider12@users.noreply.github.com>
2026-02-24 01:37:21 +0530

    Backend url for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -5,6 +5,6 @@ const API_URL =
     : import.meta.env.VITE_API_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: VITE_API_URL,
   withCredentials: true,
 });
